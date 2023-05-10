1acc1f9a099d8553f93e109c7faba7f5ca8dbdc1
mariiaviinik <mariyavinnik28@gmail.com>
2023-05-10 16:00:30 +0200

    adding hw3

---
+++ src/hw3/CalcPow.java
@@ -0,0 +1,9 @@
+package hw3;
+
+public class CalcPow {
+    public static void main(String[] args) {
+        int x = 5;
+        int n = 4;
+        System.out.println("x^n = " + Math.pow(x, n));
+    }
+}
